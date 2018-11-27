ddc8dc8fe40e71ff0f563de08f2b09440da30b12
Nicholas Dilger <dilger.nicholas@gmail.com>
2018-11-27 00:03:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 console.error("HALP!")
+console.info("I forgive you!")
+console.warn("Please return to me, i miss you. I miss the times we spent together. I dont care for your sins, because they are gone. I love you Nick, please comfort me.")
